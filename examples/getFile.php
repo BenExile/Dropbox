dbbc258a5312a26f8679d9c7e8f2548b66155797
Ben Tadiar <ben@bentadiar.co.uk>
2012-01-25 21:42:28 +0000

    Added getFile example

---
+++ examples/getFile.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Download a file and it's metadata
+ * The object returned will contain the file name, MIME type, metadata 
+ * (obtained from x-dropbox-metadata HTTP header) and file contents
+ * @link https://www.dropbox.com/developers/reference/api#files-GET
+ * @link https://github.com/BenTheDesigner/Dropbox/blob/master/Dropbox/API.php#L100-119
+ */
+
+// Require the bootstrap
+require_once('bootstrap.php');
+
+// Set the file path
+// You will need to modify $path or run putFile.php first
+$path = 'api_upload_test.txt';
+
+// Download the file
+$file = $dropbox->getFile($path);
+
+// Dump the output
+var_dump($file);
