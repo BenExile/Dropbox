6c9dc98d5589a7d6089dad87a695619dd36a7c70
Ben Tadiar <ben@bentadiar.co.uk>
2012-01-25 21:35:22 +0000

    Added metaData example

---
+++ examples/metaData.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Retreive the metadata for a file/folder
+ * @link https://www.dropbox.com/developers/reference/api#metadata
+ * @link https://github.com/BenTheDesigner/Dropbox/blob/master/Dropbox/API.php#L121-143
+ */
+
+// Require the bootstrap
+require_once('bootstrap.php');
+
+// Set the file path
+// You will need to modify $path or run putFile.php first
+$path = 'api_upload_test.txt';
+
+// Get the metadata for the file/folder specified in $path
+$metaData = $dropbox->metaData($path);
+
+// Dump the output
+var_dump($metaData);
