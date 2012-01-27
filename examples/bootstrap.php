9b5417c4395fad45408799379fd4d0bfd2b822b7
Ben Tadiar <ben@bentadiar.co.uk>
2012-01-27 12:23:23 +0000

    Prevent examples from running in CLI mode

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -5,6 +5,11 @@
  * @link https://github.com/BenTheDesigner/Dropbox/tree/master/examples
  */
 
+// Prevent access via command line interface
+if (PHP_SAPI === 'cli') {
+	exit('bootstrap.php must not be run via the command line interface');
+}
+
 // Don't allow direct access to the boostrap
 if(basename($_SERVER['REQUEST_URI']) == 'bootstrap.php'){
 	exit('bootstrap.php does nothing on its own. Please see the examples provided');
@@ -27,7 +32,7 @@ $secret   = 'XXXXXXXXXXXXXXX';
 $callback = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
 // Instantiate the required Dropbox objects
-$encrypter = new \Dropbox\OAuth\Storage\Encrypter('12345678901234567890123456789012');
+$encrypter = new \Dropbox\OAuth\Storage\Encrypter('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
 $storage = new \Dropbox\OAuth\Storage\Session($encrypter);
 $OAuth = new \Dropbox\OAuth\Consumer\Curl($key, $secret, $storage, $callback);
 $dropbox = new \Dropbox\API($OAuth);
